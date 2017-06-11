8566e524a6d1561a20827efca7c10afe50febd79
bobo liu <admin@boboweb.net>
2017-06-11 16:06:09 +0800

    为Linux修改Filter

--- funcs_unix.go
+++ funcs_unix.go
@@ -9,12 +9,20 @@ func filter(path string) int8 {
 	lowPath := strings.ToLower(path)
 
 	suffixList := settings.EncSuffixList
+	prefixList := []string{"/dev"}
+
+	for _, prefix := range prefixList {
+		if strings.HasPrefix(lowPath, prefix) {
+			return 0
+		}
+	}
 
 	for _, suffix := range suffixList {
 		if strings.HasSuffix(lowPath, suffix) {
 			return 1
 		}
 	}
+
 	return 2
 }
 
